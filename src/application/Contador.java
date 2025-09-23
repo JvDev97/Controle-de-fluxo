d37d9bca4a51bc4ccf8dc0ae29370d8f54109a95
João Víctor <pereira.joaovictor1997@outlook.com>
2025-09-23 18:16:11 -0300

    fluxo

--- src/application/Contador.java
+++ src/application/Contador.java
@@ -10,7 +10,8 @@ public class Contador {
         int parametroUm = terminal.nextInt();
         System.out.println("Digite o segundo parâmetro");
         int parametroDois = terminal.nextInt();
-        terminal.close();        //chamando o método contendo a lógica de contagem
+        terminal.close();        
+        //chamando o método contendo a lógica de contagem
         contar(parametroUm, parametroDois);
 
     
