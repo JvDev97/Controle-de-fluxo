43a203c09c13723ce2212a87d86f2e428da9132f
JvDev97 <pereira.joaovictor1997@outlook.com>
2025-09-23 15:39:26 -0300

    Controle de fluxo

---
+++ src/application/Contador.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.security.InvalidParameterException;
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) throws ParametrosInvalidosExcepition {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        //chamando o método contendo a lógica de contagem
+        contar(parametroUm, parametroDois);
+
+    
+
+    try {
+        contar(parametroUm, parametroDois);
+    } catch (ParametrosInvalidosExcepition e) {
+        System.out.println( e.getMessage());
+    }
+
+
+
+    }
+
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosExcepition {
+        //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parametroUm>=parametroDois){
+            throw new InvalidParameterException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+        int contagem = parametroDois - parametroUm;
+        //realizar o for para imprimir os números com base na variável contagem
+        for (int i=1; i<=contagem; i++){
+            System.out.println("Imprimindo número " +i);
+
+        }
+    }
+}
---
+++ src/application/ParametrosInvalidosExcepition.java
@@ -0,0 +1,7 @@
+package application;
+
+public class ParametrosInvalidosExcepition extends Exception {
+    public ParametrosInvalidosExcepition(String message) {
+        super(message);
+    }
+}
